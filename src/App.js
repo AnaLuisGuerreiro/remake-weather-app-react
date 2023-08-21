a5f79271aedd1afb198d0f85bfaee854d434b678
AnaLuisGuerreiro <124161000+AnaLuisGuerreiro@users.noreply.github.com>
2023-08-21 18:17:14 +0100

    delete error

--- src/App.js
+++ src/App.js
@@ -4,12 +4,10 @@ import "./styles.css";
 import Main from "./Main";
 import Forecast from "./Forecast";
 import Footer from "./Footer";
-import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <Weather city="Tokyo" />
       <Main />
       <Forecast />
       <Footer />
